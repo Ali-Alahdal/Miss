3ba714c482c891e5ab63fc9324218db8e5f2c727
Ali Alahdal <aloshegamer@gmail.com>
2024-08-11 18:14:55 +0300

    Complated Button Holder

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -8,39 +8,34 @@ function Main() {
 
     //Define States for The Movalbe Button to know his Current Position
     const refChild = useRef(null);
-    const [childPosition , setChildPosition] = useState();
+  
 
-    //use Effect to Track Movable Button Places
-    useEffect(()=>{
-        
-        //Get Active When Movable Button Moves and save his poition in insde state
-        function track()
-        {
-            const current = refChild.current.children[0];
-            setChildPosition({ x : current.offsetLeft , y : current.offsetTop  })
-        }
-        
-        //Track Mouse Up So We Can Get Button Posiiton
-        window.addEventListener('mouseup' , track)
-    },[refChild])
+   
 
     return ( <>
             
 
             {/* Div have Moveable Button And Button Holder To Test The Process of Tracking */}
-            {/* Dont Touch it Husam !!!!!! */}
-            <div className="position-relative w-100">
-                {/* <MoveableButton text={"Button 1"} /> */}
-                <div ref={refChild}>
-                    <MoveableButton  text={"Button 2"} name={"c1"} /> 
-                </div>
-                <ButtonHolder childPosition={childPosition}  />
-            </div>
+            {/* Husam Dont Touch it  !!!!!! */}
+            
                
+            <div className="w-100 bg-warning text-center  ">
+                <div className="w-100 align-items-center text-center">
+                    {/* <MoveableButton text={"Button 1"} /> */}
 
+
+                    <div  ref={refChild}>
+                        <MoveableButton text={"Button 2"} /> 
+                    </div>
+                    
+                    <ButtonHolder  child={refChild}  />
+                </div>
+            </div>
             
             <div className="w-100">
+
                
+
                 <Question/>
             </div>
        
