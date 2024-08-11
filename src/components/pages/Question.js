2488b0eaf33dc87f7cccb450ac0d58cfd8bec313
Ali Alahdal <aloshegamer@gmail.com>
2024-08-11 21:28:15 +0300

    layout init

--- src/components/pages/Question.js
+++ src/components/pages/Question.js
@@ -1,53 +1,38 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Data from "../../Data.json";
 
-function Question() {
-  const [resultQ, setResultQ] = useState(null);
-  const [resultA, setResultA] = useState([]);
-  const [rightA, setRightA] = useState(null);
-  const [count, setCount] = useState(0);
-  
-
-  useEffect(() => {
-    setResultQ(Data[count].question);
-    setResultA(Data[count].answers);
-    setRightA(Data[count].rightAnswer);
-  }, [count]);
-
-
-  function nextQuestion(e){
-    const myBtn = e;
-    console.log(myBtn.target.value)
-    if(myBtn.target.name == rightA){
+function Question(props) {
+    const refBtn = useRef(null);
+    const [answers , setAnswers ] = useState(props.answers);
+    const [btnStyle , setBtnStyle ] = useState("answer_btn btn btn-primary mt-5 m-3 text-light")
+    useEffect(() =>{
+      setAnswers(props.answers);
       
-      setCount(prev => prev + 1)
-    }else{
+     
+      
+    },[props.answers,props.question])
 
-      myBtn.target.className = myBtn.target.className + " broke_btn ";
-      console.log(myBtn)
-    }
-
-  }
-
+ 
 
 
   return (
     <>
-      <div className="qs_style bg-warning d-flex justify-content-center align-items-center">
-          <div className="qs_box bg-danger rounded">
-            <h2 className="text-light pt-5 text-center">{resultQ}</h2>
-            <div className="d-flex justify-content-center">
-                {
-                  resultA.map((res,i)=> (
-
-                    <button onClick={nextQuestion} name={res.value} className= "answer_btn btn btn-primary mt-5 m-3 text-light" key={i}>
-                      {res.value}
-                    </button>
-                  ))
-                }
-            </div>
-          </div>
-      </div>
+    <div className="w-100   text-center  ">
+        <h2 className="">{props.question}</h2>
+        { answers ?  answers.map((answer,index)=> {
+         
+           return(
+            <button ref={refBtn}  onClick={props.checkAnswer} name={answer.value}   className="answer_btn btn btn-primary mt-5 m-3 text-light pe-4 ps-4" key={index}>
+                {answer.value}
+              </button>
+           ) ;  
+          })
+        : null }
+    </div>  
+     
+         
+              
+       
     </>
   );
 }
