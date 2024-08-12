a1e7147a1206da21b43e2ffcd65048f03d496947
Husam Abdulraheem <ht93297@gmail.com>
2024-08-12 23:35:50 +0300

    fix surrender and Start btn

--- src/components/pages/StartMenu.js
+++ src/components/pages/StartMenu.js
@@ -1,5 +1,18 @@
 import BreakableButton from "../ButtonComponents/BreakableButton";
+import Count from "../../Context/CountContext";
+import Start from "../../Context/StartContext";
+import { useContext } from "react";
+
 function StartMenu() {
+
+    const {count, setCount} = useContext(Count);
+    const {start, setStart} = useContext(Start);
+
+    function handleStart(){
+        setCount(count + 1)
+        localStorage.setItem("start", "true")
+        setStart(true)
+    }
     return ( 
         <>
             <div className="w-100 text-center ">
@@ -14,7 +27,7 @@ function StartMenu() {
                             />
                         </li>
 
-                        <li className="me-5 ms-5 mt-5 bg-success rounded-pill  ps-5 pe-5 p-2 border-bottom border-success-subtle border-3" >
+                        <li onClick={handleStart} style={{cursor: "pointer"}} className="me-5 ms-5 mt-5 bg-success rounded-pill  ps-5 pe-5 p-2 border-bottom border-success-subtle border-3" >
 
                              Start 
                                                     
