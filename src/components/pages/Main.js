f653fb9096de4b2b79012f9f42559c7604b62afa
Ali Alahdal <aloshegamer@gmail.com>
2024-08-12 16:49:40 +0300

    Styling and Start menu inti

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,34 +1,46 @@
-import { useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import ButtonHolder from "../ButtonComponents/ButtonHolder";
 import MoveableButton from "../ButtonComponents/MoveableButton";
 import Data from "../../Data.json"
 import { Link } from "react-router-dom";
 import Question from "./Question";
+import Count from "../../Context/CountContext";
+import StartMenu from "./StartMenu";
 function Main() {
 
     //Define States for The Movalbe Button to know his Current Position
-    const refChild = useRef(null);
-  
+    const [start , setStart ] = useState(false)
+    
 
     const [currentQuestion, setCurrentQuestion] = useState("");
     const [currentAnswers, setCurrentAnswers] = useState([]);
     const [correctAnswer, setCorrectAnswer] = useState("");
-    const [count, setCount] = useState(0);
-  
+    const {count, setCount} = useContext(Count);
+    const [isCorrect, setIsCorrect] = useState(false)
 
     useEffect(() => {
         setCurrentQuestion(Data[count].question);
         setCurrentAnswers(Data[count].answers);
         setCorrectAnswer(Data[count].rightAnswer);
-    }, [count]);
+        setCount(parseInt(localStorage.getItem("count")))
+    }, [count,localStorage]);
 
 
 
     function checkAnswer(e){
         
         if(e.target.name === correctAnswer){
-          
-            setCount(prev => prev + 1);
+            setStart(true)
+            setIsCorrect(true)
+            
+            setTimeout(() => {
+                setIsCorrect(false);
+                const currentCount = count + 1;
+                setCount(prev => prev + 1);
+                localStorage.setItem("count" , parseInt( currentCount));
+                
+            }, 3 * 1000);
+            
         }else{
             console.log("wrong Answer");
         }
@@ -58,8 +70,10 @@ function Main() {
             </div> */}
             
             <div className="w-75 m-auto mt-4 h-75 d-flex align-items-center text-center" style={{backgroundColor:"#c47d51"}}>
-
-                <Question question={currentQuestion} checkAnswer={checkAnswer} answers={currentAnswers} st={""} />
+                {start ? <Question question={currentQuestion} checkAnswer={checkAnswer} answers={currentAnswers} isCorrect={isCorrect} />
+                :
+                <StartMenu/>}
+                
                
             </div>
        
---
+++ src/components/pages/StartMenu.js
@@ -0,0 +1,18 @@
+function StartMenu() {
+    return ( 
+        <>
+            <div className="w-100 text-center">
+                <h1>Game Name</h1>
+                <div className="d-flex m-auto">
+                    <ul className="list-unstyled d-flex m-auto ">
+                        <li> Start </li>
+                        <li> Start </li>
+                        <li> Start </li>
+                    </ul>
+                </div>
+            </div>
+        </>
+     );
+}
+
+export default StartMenu;
